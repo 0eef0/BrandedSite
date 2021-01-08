8f219c5b8dd3526e14a981c34813ac88a01cb927
TheCapn-MEC <erolda266@west-mec.org>
2021-01-08 11:43:24 -0700

    finished shop

--- js/scripts.js
+++ js/scripts.js
@@ -11,15 +11,23 @@ function createItem(img, name, price, description){
     '                            <p>$' + price + '</p>'+
     '                            <p>' + description + '</p>'+
     '                        </td>'+
-    '                        <td style="vertical-align: bottom;">'+
+    '                        <td style="vertical-align: bottom; text-align: right;">'+
     '                            <button><p>Add to Cart</p></button>'+
     '                        </td>'+
     '                    </tr>'+
     '                </table>';
-    testNum++;
-    console.log(testNum);
 }
-var testNum = 0;
+function prepareShop(){
+    //createItem("image name, name of product, price, description")
+    createItem("shirt", "Fashionable T-Shirt", 15, "Hop on the trend of wearing shirts that cover up hardly anything by purchasing this shirt!");
+    createItem("ancient tech", "Ancient Relic", 50, "This is an old one. We have no idea when it is from, all we know is that it runs at a tolerable level.");
+    createItem("toys", "Lot of Childrens Toys", 20, "These were found behind a local McDonalds, we are sure that any child would love to have them.");
+    createItem("brick", "Medieval Weaponry", 25, "We are 90% sure that this was used to commit armed robbery, so if you wish to be able to defend yourself at moments notice, this is what you need.");
+    createItem("liquid", "Mystery Liquid", 10, "Chemical tests have come back saying that this liquid is a nonlethal substance. So summon your sense of adventure and find out what his is by buying it!");
+    createItem("sweater", "Christmas Sweater", 5, "We found this one on exactly December 26th, 2020. Our guess is that it is a Christmas Gift that someone just did not appreciate enough. Show this sweater that love it deserves and buy it now!");
+}
 
-createItem("shirt", "Fashionable T-Shirt", 15, "Hop on the trend of wearing shirts that cover up hardly anything by purchasing this shirt!");
-createItem("big logo", "test2", 400, "this is a description")
+//function below sets up each item in the cart
+function createCartItem(img, name, price){
+
+}
